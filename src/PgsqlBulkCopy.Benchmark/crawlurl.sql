0de982da534b10c52eec3e60f2763f9cb52b8f2a
段秋华 <duanqiuhua@gridsum.com>
2018-05-28 20:16:52 +0800

    add src dir

---
+++ src/PgsqlBulkCopy.Benchmark/crawlurl.sql
@@ -0,0 +1,28 @@
+﻿-- Table: public.crawlurl
+
+-- DROP TABLE public.crawlurl;
+
+CREATE TABLE public.crawlurl
+(
+  id integer NOT NULL DEFAULT nextval('crawlurl_id_seq'::regclass),
+  code character(32),
+  url text,
+  sort integer,
+  createtime timestamp without time zone,
+  CONSTRAINT crawlurl_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.crawlurl
+  OWNER TO postgres;
+
+-- Index: public.idx_code
+
+-- DROP INDEX public.idx_code;
+
+CREATE INDEX idx_code
+  ON public.crawlurl
+  USING btree
+  (code COLLATE pg_catalog."default");
+
